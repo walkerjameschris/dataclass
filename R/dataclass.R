8fbf98fc33b90f21131144859dbbf7fff88b94a8
Chris <127646133+walkerjameschris@users.noreply.github.com>
2023-03-20 11:34:56 -0400

    Update dataclass.R

--- R/dataclass.R
+++ R/dataclass.R
@@ -168,10 +168,10 @@ dataclass <- function(...) {
     }
     
     if (length(error) >= 1) {
-      cli::cli_abort(c(
+      return(cli::cli_abort(c(
         "The following elements have error-level violations:",
         purrr::set_names(error, "x")
-      ))
+      )))
     }
     
     inputs_to_validate
